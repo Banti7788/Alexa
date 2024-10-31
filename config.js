44f1f33cc62b687295e456148362d4cefed44ea7
Banti7788 <panelbanti562@gmail.com>
2024-10-31 18:28:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "JJ412IjL#mcKo6nhN4ylz7xzrXfbX9Pe07QUxBo7e_1hQIdo_W4E",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
